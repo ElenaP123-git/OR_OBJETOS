c2eee5bd25577490f4a924aa1971e7a798235003
Elena123-git <epabben544>
2026-02-09 09:28:31 +0100

    subo

---
+++ src/Orientacion_Obj/Boletin1/DatosClima/RegistroDiario.java
@@ -0,0 +1,35 @@
+package Orientacion_Obj.Boletin1.DatosClima;
+import java.time.LocalDate;
+
+public class RegistroDiario {
+
+    //atributos
+
+    Datos datos;
+    LocalDate fecha;
+    String lugarRecogida;
+
+    //constructor
+
+    public RegistroDiario(Datos datos, LocalDate fecha, String lugarRecogida) {
+        this.datos = datos;
+        this.fecha = fecha;
+        this.lugarRecogida = lugarRecogida;
+    }
+
+    //métodos
+
+    public double calcularTemperaturaMediaDia() {
+        return datos.calculo_temp_media();
+    }
+
+    @Override
+    public String toString() {
+        return "RegistroDiario{" +
+                "datos=" + datos +
+                ", fecha=" + fecha +
+                ", lugarRecogida='" + lugarRecogida + '\'' +
+                '}';
+    }
+}
+
