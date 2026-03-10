1254200b1cc432f274dddb080edebd8902ae6e49
Elena123-git <epabben5442g.educaand.es>
2026-03-10 19:46:34 +0100

    subo

--- src/Excepciones/boletin2/Ej2/Models/Recetario.java
+++ src/Excepciones/boletin2/Ej2/Models/Recetario.java
@@ -30,16 +30,16 @@ public class Recetario {
 
             for (int i = 0; i < numRecetas; i++) {
 
-                // condición contraria: si NO coincide el nombre, seguimos
+                // si NO coincide el nombre, seguimos (de otra manera me da error)
                 if (!recetas[i].getNombre().equalsIgnoreCase(nombre)) {
                     continue;
                 }
 
-                // si coincide, la devolvemos
+                // si coincide, de lujo, pa lante
                 return recetas[i];
             }
 
-            // si termina el bucle sin encontrarla, lanzamos tu excepción
+            // si termina el bucle sin encontrarla, lanzo la excepción (esto pasa cuando receta es null)
             throw new CocinaException("\nLa receta " + nombre + " no existe en el recetario");
         }
 
